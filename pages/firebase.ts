94513e3e606089f83e7ccf5bd26f4ebd55a60b03
Milo R. Jørgensen <mrj@holion.dk>
2023-11-11 13:53:04 +0100

    a

--- pages/firebase.ts
+++ pages/firebase.ts
@@ -4,7 +4,7 @@ export const firebase = admin.initializeApp({
   credential: admin.credential.cert(
     {
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
     }
   ),
