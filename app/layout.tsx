862fdc381acce38f87811c3333e0455463d0c563
Milo R. Jørgensen <mrj@holion.dk>
2023-11-13 23:30:42 +0100

    Edit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Milo subscription",
+    title: "Milo competition",
     description: "Subscribe here",
 };
 
--- components/competition-form.tsx
+++ components/competition-form.tsx
@@ -11,14 +11,15 @@ interface Props {
 export function CompetitionForm(props: Props) {
     return (
         <section className="flex-shrink">
-            <h1 className="text-6xl font-bold bg-transparent text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">Hello</h1>
+            <h1 className="text-6xl font-bold bg-transparent text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">Welcome</h1>
             {props.rolled ? (
                 <p>You have already been rick-rolled 🥳</p>
             ) : (
                 <>
-                    <p className="text-xl w-full my-4">Please enter your name below to enter the competition 👇</p>
+                    <p className="text w-full mb-3">... to Milo's competition</p>
+                    <p className="text-xl w-full mt-6 mb-3 ">Please enter your name below to enter the competition 👇</p>
                     <form className="w-full space-y-2" onSubmit={props.callback}>
-                        <Input className="w-full mb-2" placeholder="Enter your name" type="text" name="name" />
+                        <Input className="w-full mb-2" placeholder="Bob..." type="text" name="name" />
                         <Button type="submit">Enter</Button>
                     </form>
                 </>
