28ddf749c0026592ae227de675b927ba4f3e88da
Milo R. Jørgensen <mrj@holion.dk>
2023-11-13 21:51:41 +0100

    dsad

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-// import NamesListFetcher from "@/components/names-list-fetcher";
+import NamesListFetcher from "@/components/names-list-fetcher";
 import TopSection from "@/components/top-section";
 
 export default function Home() {
     return (
         <section className="flex flex-col h-screen mx-auto max-w-sm py-10">
             <TopSection/>
-            {/* <NamesListFetcher /> */}
+            <NamesListFetcher />
         </section>
     );
 }
