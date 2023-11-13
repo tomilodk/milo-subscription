8b2d94748965fac28107ec09993a297656a6b588
Milo R. Jørgensen <mrj@holion.dk>
2023-11-13 22:58:17 +0100

    bla

--- lib/names.ts
+++ lib/names.ts
@@ -1,11 +1,12 @@
+import { cache } from "react";
 import { firebase } from "./firebase";
 
 export const dynamic = "force-dynamic";
 export const fetchCache = "only-no-store";
 
-export const getNames = async (): Promise<string[]> => {
+export const getNames = cache(async (): Promise<string[]> => {
     const snapshot = await firebase().firestore().collection('names').get();
     const names = snapshot.docs.map((doc) => doc.data().name);
 
     return names;
-}
+});
