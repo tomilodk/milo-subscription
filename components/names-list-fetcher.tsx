e3c673fceaa1f1d1930e37708e04be069f286b24
Milo R. Jørgensen <mrj@holion.dk>
2023-11-13 23:06:01 +0100

    bub

--- components/names-list-fetcher.tsx
+++ components/names-list-fetcher.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import NamesList from "./names-list";
 import { getNames } from "@/lib/names";
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "only-no-store";
+
 export default async function NamesListFetcher() {
     const users = await getNames();
 
