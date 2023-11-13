ea1f4165c3579bb4f9ae42fe01bb6de4bda155aa
Milo R. Jørgensen <mrj@holion.dk>
2023-11-13 22:31:47 +0100

    Suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import NamesListFetcher from "@/components/names-list-fetcher";
 import TopSection from "@/components/top-section";
+import { Suspense } from "react";
 
 export default function Home() {
     return (
         <section className="flex flex-col h-screen mx-auto max-w-sm py-10">
-            <TopSection/>
-            <NamesListFetcher />
+            <TopSection />
+            <Suspense fallback={<p>Loading...</p>}>
+                <NamesListFetcher />
+            </Suspense>
         </section>
     );
 }
