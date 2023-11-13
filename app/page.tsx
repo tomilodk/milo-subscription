45c1ddf46215e56ed0d4b69e3ad3cf538aa738e5
Milo R. Jørgensen <mrj@holion.dk>
2023-11-13 23:09:57 +0100

    bub

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import NamesListFetcher from "@/components/names-list-fetcher";
 import TopSection from "@/components/top-section";
 import { Suspense } from "react";
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "only-no-store";
+
 export default async function Home() {
     return (
         <section className="flex flex-col h-screen mx-auto max-w-sm py-10">
--- lib/names.ts
+++ lib/names.ts
@@ -1,8 +1,5 @@
 import { firebase } from "./firebase";
 
-export const dynamic = "force-dynamic";
-export const fetchCache = "only-no-store";
-
 export const getNames = async (): Promise<string[]> => {
     const snapshot = await firebase().firestore().collection('names').get();
     const names = snapshot.docs.map((doc) => doc.data().name);
